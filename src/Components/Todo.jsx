00fcc3ad66ad7747895637d8e93b12b9e65772bb
ochieng clement <odokclement@gmail.com>
2025-08-26 22:06:52 +0300

    fixed merge issues

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import  { useEffect, useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import Todoitems from "./Todoitems";
 
